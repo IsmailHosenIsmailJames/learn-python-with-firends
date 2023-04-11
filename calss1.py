62b0fc40aef76e56fe53d74c9563fe094ae474a7
MD. Ismail Hosen James <md.ismailhosenismailjames@gmail.com>
2023-04-11 23:24:22 +0600

    code solved

--- calss1.py
+++ calss1.py
@@ -1,39 +1,21 @@
-ans = 0
-
-def myFunction(ex: str):
-    global ans
-    spdt = ex.split(" ")
-    if (spdt[0] == "ans" or spdt[0] == "Ans"):
-        num1 = ans
-    else:
-        num1 = float(spdt[0])
-
-    oparation = spdt[1]
-
-    if (spdt[2] == "ans" or spdt[2] == "Ans"):
-        num2 = ans
-    else:
-        num2 = float(spdt[2])
-
-    if (oparation == '/'):
+def myFunction(num1:float, oparation:str, num2:float):
+    if(oparation == '/'):
         result = num1 / num2
-    elif (oparation == '*'):
+    elif(oparation == '*'):
         result = num1 * num2
-    elif (oparation == '+'):
+    elif(oparation == '+'):
         result = num1 + num2
-    elif (oparation == '-'):
+    elif(oparation == '-'):
         result = num1 - num2
-    elif (oparation == '**'):
+    elif(oparation == '**'):
         result == num1 ** num2
-    elif (oparation == '%'):
+    elif(oparation == '%'):
         result = num1 % num2
-    elif (oparation == 'root'):
+    elif(oparation == 'root'):
         result = num1 ** (1 / num2)
-
-    ans = result
+        
     return result
 
-
 while True:
-    x = myFunction(input())
-    print("=", x)
+    x = myFunction(float(input()), input(), float(input()))
+    print(x)
